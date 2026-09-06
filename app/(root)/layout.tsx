fe1ad23eb8f5d7a050723aabc10f8fa9da81583c
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-06 13:56:01 +0430

    add header component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/shared/header";
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <div className="flex h-screen flex-col">
-      <main className="flex-1 max-w-7xl p-5 px-10 w-full">
+      <Header />
+      <main className="flex-1 max-w-7xl p-5 px-10 w-full mx-auto">
         {children}
       </main>
     </div>
---
+++ components/shared/header/index.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="w-full border-b">
+            <div className="flex justify-between items-center flex-1 max-w-7xl p-5 px-10 w-full mx-auto">
+                <div className="flex justify-start items-center">
+                    <Link href='/' className="flex justify-start items-center">
+                        <span>لوگو</span>
+                        <span className="block font-bold text-2xl mr-3">فروشگاه</span>
+                    </Link>
+                </div>
+                <div className="space-x-2">
+                    <Button asChild>
+                        <Link href='/cart'>
+                            <ShoppingCart />سبد خرید
+                        </Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href='/sign-in'>
+                            <UserIcon />حساب کاربری
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </header>
+    )
+}
