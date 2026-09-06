d782f47d46540b7021a0ca783f29ee5ca625351f
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-06 10:50:42 +0430

    add local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Shopping App",
   description: "Shopping App",
 };
 
+const kalameFont = localFont({
+  src: '../public/font/Kalameh-Regular.ttf'
+})
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html>
-      <body className="antialiased">{children}</body>
+      <body className={`${kalameFont.className} antialiased`}>{children}</body>
     </html>
   );
 }
