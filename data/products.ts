b9da618c5eaeba1505571eb37b838ff3759512e5
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-08 14:03:01 +0430

    add test products

---
+++ data/products.ts
@@ -0,0 +1,46 @@
+const data = {
+    products: [
+        {
+            name: 'دستمال مرطوب Adonis',
+            slug: 'دستمال-مرطوب-Adonis',
+            category: 'دستمال موطوب',
+            description: 'پاک کنندگی قوی ، قادر به پاک کردن انواع آرایش ، مرطوب کننده پوست ، ضد حساسیت  ، 20 برگ',
+            image: '/images/wipes1.jpeg',
+            price: 109,
+            brand: 'Adonis',
+            stock: 5,
+        },
+        {
+            name: 'کرم آبرسان دست و صورت پمپیfree face',
+            slug: 'کرم-آبرسان-دست-و-صورت-پمپی',
+            category: 'کرم آبرسان',
+            description: 'مرطوب کننده دست و صورت  ، نرم کننده پوست های خشک ،استفاده روزانه  ، آبرسان فوق العاده ،حجم 500 میل با صرفه ، مناسب برای انواع پوست',
+            image: '/images/crem1.jpeg',
+            price: 129,
+            brand: 'free face',
+            stock: 7,
+        },
+        {
+            name: 'رژ جامد NOTE',
+            slug: 'رژ-جامد-NOTE',
+            category: 'رژ جامد',
+            description: 'بافت مخملی ، پیگمنت عالی ، بافت نرم و روان ، رایحه فوق العاده',
+            image: '/images/rozh1.jpeg',
+            price: 159,
+            brand: 'NOTE',
+            stock: 3,
+        },
+        {
+            name: 'اسپری ماسک مو 10 کاره FREE FACE',
+            slug: 'اسپری-ماسک-مو-10-کاره-FREE-FACE',
+            category: 'اسپری',
+            description: '• ترمیم موهای خشک و آسیب‌دیده • کاهش وز و گره‌خوردگی • افزایش نرمی و لطافت  • محافظت حرارتی در برابر سشوار و اتو • کمک به درخشندگی مو • جلوگیری از موخوره • حالت‌پذیری بهتر مو • سبک، بدون ایجاد سنگین',
+            image: '/images/spray1.jpeg',
+            price: 189,
+            brand: 'NOTE',
+            stock: 8,
+        },
+    ]
+}
+
+export default data
