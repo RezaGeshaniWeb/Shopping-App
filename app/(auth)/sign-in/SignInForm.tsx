13ef6df2efabcff4ce5a77cbcabb6836a80b0780
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-15 16:05:42 +0430

    create sign-in form

--- app/(auth)/sign-in/SignInForm.tsx
+++ app/(auth)/sign-in/SignInForm.tsx
@@ -1,7 +1,29 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
 export default function SignInForm() {
     return (
-        <div>
-            SignIn Form
-        </div>
+        <>
+            <div className="space-y-6">
+                <div>
+                    <Label className="mb-2" htmlFor="email">ایمیل</Label>
+                    <Input id="email" name="email" type="email" required autoComplete="email" />
+                </div>
+                <div>
+                    <Label className="mb-2" htmlFor="password">رمز عبور</Label>
+                    <Input id="password" name="password" type="password" required autoComplete="password" />
+                </div>
+                <div>
+                    <Button className="w-full" variant={"default"}>ورود</Button>
+                </div>
+                <div className="text-sm text-center">
+                    <Link href={'/sign-up'}>ثبت نام</Link>
+                </div>
+            </div>
+        </>
     )
 }
