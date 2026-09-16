520cb7fd8956790a2809c3889476b111894f5b5d
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-16 08:49:19 +0430

    edit form styles

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,12 +4,12 @@ import SignInForm from "./SignInForm";
 
 export default function SignInPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center">
+        <div className="min-h-screen flex items-center justify-center w-full h-full">
             <Card className="w-full max-w-md">
                 <CardHeader className="space-y-4">
                     <Link href={'/'}>بازگشت به فروشگاه</Link>
                     <CardTitle className="text-center">ورود</CardTitle>
-                    <CardDescription>ورود به حساب کاربری</CardDescription>
+                    <CardDescription className="text-center">ورود به حساب کاربری</CardDescription>
                 </CardHeader>
                 <CardContent className="space-y-4">
                     <SignInForm />
