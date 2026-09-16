39952cbaa775902ef40c64d6479767d464c151e7
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-16 23:29:31 +0430

    action fn for add to cart

--- components/shared/product/AddToCart.tsx
+++ components/shared/product/AddToCart.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import { addItemToCart } from "@/lib/actions/cart.actions"
 import { CartItem } from "@/types"
 
 export default function AddToCart({ item }: { item: CartItem }) {
-    async function handleAddToCart() { }
+    async function handleAddToCart() { 
+        const response = await addItemToCart(item)
+    }
 
     return (
         <Button className="w-full" type="button" onClick={handleAddToCart}>افزودن به سبد خرید</Button>
