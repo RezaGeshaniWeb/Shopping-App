a021a7957b0f16c9f374e408b32945e8e6c5b0fa
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-16 23:52:48 +0430

    get item from cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -3,16 +3,20 @@
 import { auth } from "@/auth";
 import { CartItem } from "@/types";
 import { cookies } from "next/headers";
+import { prisma } from "../prisma";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
     try {
         const sessionCartId = (await cookies()).get('sessionCartId')?.value
-
         if (!sessionCartId) throw new Error('Session not found.')
-
         const session = await auth()
-
         const userId = session?.user?.id ? (session.user.id as string) : undefined
+        const cart = await getMyCart()
+        const item = cartItemSchema.parse(data)
+        const product = await prisma.product.findFirst({
+            where: { id: item.productId }
+        })
 
         return {
             success: true,
@@ -23,5 +27,24 @@ export async function addItemToCart(data: CartItem) {
             success: false,
             message: error,
         }
+    }
+}
+
+export async function getMyCart() {
+    const sessionCartId = (await cookies()).get('sessionCartId')?.value
+    if (!sessionCartId) throw new Error('Session not found.')
+    const session = await auth()
+    const userId = session?.user?.id ? (session.user.id as string) : undefined
+    const cart = await prisma.cart.findFirst({
+        where: userId ? { userId } : { sessionCartId }
+    })
+    if (!cart) return undefined
+
+    return {
+        ...cart,
+        items: cart.items as CartItem[],
+        itemsPrice: cart.itemsPrice.toString(),
+        totalPrice: cart.totalPrice.toString(),
+        shippingPrice: cart.shippingPrice.toString(),
     }
 }
