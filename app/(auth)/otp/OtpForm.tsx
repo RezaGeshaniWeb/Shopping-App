8e6501cfdccd58d4ca2dc9c3e937e5535dad176c
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-16 14:23:57 +0430

    get OTP code

--- app/(auth)/otp/OtpForm.tsx
+++ app/(auth)/otp/OtpForm.tsx
@@ -67,7 +67,45 @@ export default function OtpForm() {
 
     return (
         <>
-            <form onSubmit={submitHandler}></form>
+            <form onSubmit={submitHandler}>
+                <div className="space-y-6">
+                    {isStepTwo ? (
+                        <div>
+                            <Label className="mb-2">کد تایید</Label>
+                            <Input
+                                id="vCode"
+                                name="vCode"
+                                type="text"
+                                value={userCode}
+                                disabled={loginInProgress}
+                                onChange={e => setUserCode(e.target.value)}
+                                required
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <Label className="mb-2">موبایل</Label>
+                            <Input
+                                id="mobile"
+                                name="mobile"
+                                type="text"
+                                value={mobile}
+                                disabled={loginInProgress}
+                                onChange={e => setMobile(e.target.value)}
+                                required
+                            />
+                        </div>
+                    )}
+                    <Button type="submit" className="w-full" variant={"default"} disabled={loginInProgress}>
+                        {isStepTwo ? <span>ثبت کد</span> : <span>ورود</span>}
+                    </Button>
+                    {error && (
+                        <div className="text-center">
+                            <span>کد وارد شده اشتباه است</span>
+                        </div>
+                    )}
+                </div>
+            </form>
         </>
     )
 }
