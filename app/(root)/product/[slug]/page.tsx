9b407f90e537dbb306f361c3552f2219faa02ece
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-17 00:42:15 +0430

    variable for add to cart button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import AddToCart from "@/components/shared/product/AddToCart"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
+import { getMyCart } from "@/lib/actions/cart.actions"
 import { getProductBySlug } from "@/lib/actions/product.actions"
 import Image from "next/image"
 import { notFound } from "next/navigation"
@@ -9,8 +9,8 @@ import { notFound } from "next/navigation"
 export default async function ProductPage(props: { params: Promise<{ slug: string }> }) {
     const { slug } = await props.params
     const product = await getProductBySlug(slug)
-
     if (!product) notFound()
+    const cart = await getMyCart()
 
     return (
         <>
@@ -49,7 +49,7 @@ export default async function ProductPage(props: { params: Promise<{ slug: strin
                                 </div>
                                 {product.stock > 0 && (
                                     <div className="flex items-center">
-                                        <AddToCart item={{
+                                        <AddToCart cart={cart} item={{
                                             productId: product.id,
                                             name: product.name,
                                             slug: product.slug,
--- components/shared/product/AddToCart.tsx
+++ components/shared/product/AddToCart.tsx
@@ -1,15 +1,32 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { addItemToCart } from "@/lib/actions/cart.actions"
-import { CartItem } from "@/types"
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions"
+import { Cart, CartItem } from "@/types"
+import { Minus, Plus } from "lucide-react";
 
-export default function AddToCart({ item }: { item: CartItem }) {
-    async function handleAddToCart() { 
+export default function AddToCart({ cart, item }: { cart?: Cart; item: CartItem }) {
+    async function handleAddToCart() {
         const response = await addItemToCart(item)
     }
 
-    return (
+    async function handleRemoveFromCart() {
+        const response = await removeItemFromCart(item.productId)
+    }
+
+    const existItem = cart && cart.items.find((p) => p.productId === item.productId)
+
+    return existItem ? (
+        <div>
+            <Button type="button" variant={"outline"} onClick={handleRemoveFromCart}>
+                <Minus className="w-4 h-4" />
+            </Button>
+            <span className="px-2">{existItem.qty}</span>
+            <Button type="button" variant={"outline"} onClick={handleAddToCart}>
+                <Plus className="w-4 h-4" />
+            </Button>
+        </div>
+    ) : (
         <Button className="w-full" type="button" onClick={handleAddToCart}>افزودن به سبد خرید</Button>
     )
 }
