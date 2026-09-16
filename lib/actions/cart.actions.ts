63825b65838d9942e3e762abf8a397d74524cc81
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-16 23:41:56 +0430

    get session

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,10 +1,27 @@
 'use server'
 
+import { auth } from "@/auth";
 import { CartItem } from "@/types";
+import { cookies } from "next/headers";
 
 export async function addItemToCart(data: CartItem) {
-    return {
-        success: true,
-        message: 'محصول به سبد خرید اضافه شد',
+    try {
+        const sessionCartId = (await cookies()).get('sessionCartId')?.value
+
+        if (!sessionCartId) throw new Error('Session not found.')
+
+        const session = await auth()
+
+        const userId = session?.user?.id ? (session.user.id as string) : undefined
+
+        return {
+            success: true,
+            message: 'محصول به سبد خرید اضافه شد',
+        }
+    } catch (error) {
+        return {
+            success: false,
+            message: error,
+        }
     }
 }
