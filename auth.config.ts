5916d9725345981085c4442ac9adfa7306576ad7
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-19 14:35:35 +0430

    auth management

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,6 @@
 import type { NextAuthConfig } from "next-auth";
 import { NextResponse } from "next/server";
+import { auth } from "./auth";
 
 export default {
     pages: {
@@ -9,18 +10,31 @@ export default {
     providers: [],
     callbacks: {
         authorized({ request }: any) {
-            if (!request.cookies.get("sessionCartId")) {
-                const sessionCartId = crypto.randomUUID();
-                const newRequestHeaders = new Headers(request.headers);
-                const response = NextResponse.next({
-                    request: {
-                        headers: newRequestHeaders,
-                    },
-                });
-                response.cookies.set("sessionCartId", sessionCartId);
-                return response;
-            }
-            return true;
+            const protectedPaths = [
+                /\/shipping/,
+                /\/payment-method/,
+                /\/place-order/,
+                /\/profile/,
+                /\/user\/(.*)/,
+                /\/order\/(.*)/,
+                /\/admin/,
+            ]
+            const { pathname } = request.nextUrl
+            if (!auth && protectedPaths.some((p) => p.test(pathname)))
+                return false
+
+            // if (!request.cookies.get("sessionCartId")) {
+            //     const sessionCartId = crypto.randomUUID();
+            //     const newRequestHeaders = new Headers(request.headers);
+            //     const response = NextResponse.next({
+            //         request: {
+            //             headers: newRequestHeaders,
+            //         },
+            //     });
+            //     response.cookies.set("sessionCartId", sessionCartId);
+            //     return response;
+            // }
+            // return true;
         },
     },
 } satisfies NextAuthConfig;
