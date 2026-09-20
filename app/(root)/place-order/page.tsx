a3e6abc826579c0c167933c15adf478b511caa7a
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-09-20 14:30:56 +0430

    order details

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -22,6 +22,66 @@ export default async function PlaceOrderPage() {
     return (
         <>
             <CheckoutSteps current={3} />
+            <h1 className="py-4 text-2xl">ثبت سفارش</h1>
+            <div className="grid grid-cols-3 gap-5">
+                <div className="col-span-2 overflow-x-auto space-y-4">
+                    <Card>
+                        <CardContent className="p-4 gap-4">
+                            <h2 className="text-xl pb-4">آدرس</h2>
+                            <p>{user?.paymentMethod}</p>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent className="p-4 gap-4">
+                            <h2 className="text-xl pb-4">سفارش ها</h2>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead className="text-right">محصول</TableHead>
+                                        <TableHead className="text-center">تعداد</TableHead>
+                                        <TableHead className="text-left">قیمت</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {cart.map((item) => (
+                                        <TableRow key={item.slug}>
+                                            <TableCell className="text-right">
+                                                <Link href={`/product/${item.slug}`} className="flex items-center gap-2">
+                                                    <Image src={item.image} alt={item.name} width={50} height={50} className="rounded-md" />
+                                                </Link>
+                                            </TableCell>
+                                            <TableCell className="text-center">
+                                                <span className="px-2">{item.qty}</span>
+                                            </TableCell>
+                                            <TableCell className="text-left">
+                                                {item.price} تومان
+                                            </TableCell> 
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div>
+                    <Card>
+                        <CardContent className="p-4 gap-4 space-y-4">
+                            <div className="flex justify-between">
+                                <div>قیمت محصولات</div>
+                                <div>{cart.itemsPrice}</div>
+                            </div>
+                            <div className="flex justify-between">
+                                <div>هزینه ارسال</div>
+                                <div>{cart.shippingPrice}</div>
+                            </div>
+                            <div className="flex justify-between">
+                                <div>مجموع هزینه</div>
+                                <div>{cart.totalPrice}</div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </>
     )
 }
